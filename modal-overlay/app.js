2406d2bd8c4cb38d6d614c4eb800f8eb8f8b8308
mdiallo98 <mamadou1237@gmail.com>
2022-03-07 05:21:18 -0500

    front end eval modal overlay 8

--- modal-overlay/app.js
+++ modal-overlay/app.js
@@ -4,10 +4,16 @@ let container = document.querySelector('.container');
 showBtn.addEventListener('click', (e) => {
   //   modal.document.style.display = 'initial';
   modal.classList.add('open-model');
+  showBtn.classList.add('hide-btn');
+  container.classList.add('bgr-color');
   console.log('clicked me');
 });
 let closeModal = document.querySelector('.close-button');
 
 closeModal.addEventListener('click', (e) => {
   modal.classList.remove('open-model');
+  showBtn.classList.remove('hide-btn');
+  container.classList.remove('bgr-color');
 });
+
+const acceptOffer = document.querySelector('.accept-btn');
