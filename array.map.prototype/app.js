1809d492391246ae57f1d992419fe59f2790a720
mdiallo98 <mamadou1237@gmail.com>
2022-03-07 03:55:32 -0500

    front end eval modal overlay 4

--- array.map.prototype/app.js
+++ array.map.prototype/app.js
@@ -3,46 +3,38 @@
 // * 1. It accepts an array, 2. it accpets some callback function.
 // * Inside of our map array we need to take in an array and for each element in that array we need to perfom the call back on it and then return a new array
 
-
-
-
-
-
-
-
 const arrayMap = (arr, callBack) => {
   let newArr = [];
-    // if(arr.length ===0){
-    //     return undefined
-    // }
+  // if(arr.length ===0){
+  //     return undefined
+  // }
 
   for (let each of arr) {
-      newArr.push(callBack(each))
+    newArr.push(callBack(each));
   }
 
   return newArr;
 };
 
-function Addnums(x){
-    return x * "200"
+function Addnums(x) {
+  return x * '200';
 }
 
-let nums =[2,3,4,5,6]
-console.log(arrayMap(nums,Addnums))
-// ** the above methode works. However it isnt a method just a function To create our own methode we need to use the Array.prototype to create a custom arry method. 
+let nums = [2, 3, 4, 5, 6];
+console.log(arrayMap(nums, Addnums));
+// ** the above methode works. However it isnt a method just a function To create our own methode we need to use the Array.prototype to create a custom arry method.
 
+const Maps = function (callBack) {
+  let newList = [];
+  for (let i = 0; i < this.length; i++) {
+    newList.push(callBack(this[i]));
+  }
 
-const Map = (callBack) {
-    let newList = []
-    for (let i =0; i<this.length; i++){
+  return newList;
+};
+Array.prototype.myMAP = Maps;
 
-        newList.push(callBack(this[i]), i)
-    }
+console.log(nums.myMAP((num) => num++));
 
-    return newList
-}
-
-
-Array.prototype.myMAP =arrayMap
-
-// nums.myMAP()
+// ** Try and implement the filter function
+// ** Filter brings back a new array
